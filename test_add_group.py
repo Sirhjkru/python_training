fc5e097c7ca7d1ba515d95d3abaad8b8bd046fe6
admin <sirhjkru@gmail.com>
2020-03-21 20:25:11 +0300

    Убрал лишние комментарии

--- test_add_group.py
+++ test_add_group.py
@@ -29,11 +29,9 @@ class TestAddGroup(unittest.TestCase):
         self.logout(wd)
 
     def logout(self, wd):
-        # logout
         wd.find_element_by_link_text("Logout").click()
 
     def return_to_groups(self, wd):
-        # return to groups page
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
@@ -53,11 +51,9 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("submit").click()
 
     def open_groups_page(self, wd):
-        # open graups page
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
-        # Login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -68,7 +64,6 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
-        # open home page
         wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
