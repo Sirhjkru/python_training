85f639a972431005cde53bed47dfba58aa9e4723
admin <sirhjkru@gmail.com>
2020-03-27 01:07:00 +0300

    добавил новый класс для реквизитов компании

---
+++ company_details.py
@@ -0,0 +1,6 @@
+class CompanyDetails():
+    def __init__(self, header, comName, comAddress, comHome):
+        self.header = header
+        self.comName =comName
+        self.comAddress = comAddress
+        self.comHome = comHome
