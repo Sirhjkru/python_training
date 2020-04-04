83b819ad2e41507f19a092fcdaad80aeb9c6c8ce
admin <sirhjkru@gmail.com>
2020-04-04 20:23:07 +0300

    Создал отдельные классы для хранения всех данных контакта.

--- fixture/application.py
+++ fixture/application.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-import os
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(10)
 
     def logout(self):
         wd = self.wd
@@ -51,17 +50,17 @@ class Application:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def adding_secondary_data(self, address="address", phone="home", notes="notes"):
+    def adding_secondary_data(self, company_details):
         wd = self.wd
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(address)
+        wd.find_element_by_name("address2").send_keys(company_details.address)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(phone)
+        wd.find_element_by_name("phone2").send_keys(company_details.phone)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(notes)
+        wd.find_element_by_name("notes").send_keys(company_details.notes)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def select_a_group(self):
@@ -70,59 +69,59 @@ class Application:
         Select(wd.find_element_by_name("new_group")).select_by_visible_text("New_group")
         wd.find_element_by_xpath("(//option[@value='12'])[3]").click()
 
-    def add_date_anniversary(self, a_day="8", a_month="August", a_year="1996"):
+    def add_date_anniversary(self, company_details):
         wd = self.wd
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(a_day)
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(company_details.a_day)
         wd.find_element_by_xpath("(//option[@value='8'])[2]").click()
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(a_month)
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(company_details.a_month)
         wd.find_element_by_xpath("(//option[@value='August'])[2]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(a_year)
+        wd.find_element_by_name("ayear").send_keys(company_details.a_year)
 
-    def add_date_birth(self, day="3", month="July", year="1997"):
+    def add_date_birth(self, company_details):
         wd = self.wd
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(day)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(company_details.day)
         wd.find_element_by_xpath("//option[@value='9']").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(month)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(company_details.month)
         wd.find_element_by_xpath("//option[@value='July']").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(year)
+        wd.find_element_by_name("byear").send_keys(company_details.year)
 
-    def add_homepage(self, homepage="vk.ru"):
+    def add_homepage(self, company_details):
         wd = self.wd
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(homepage)
+        wd.find_element_by_name("homepage").send_keys(company_details.homepage)
 
-    def add_email(self, email_one="asdfq@gmail.com", email2="cgetzdbf@yandex.com", email3="cvntxdvrc@mail.com"):
+    def add_email(self, company_details):
         wd = self.wd
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email_one)
+        wd.find_element_by_name("email").send_keys(company_details.email_1)
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(email2)
+        wd.find_element_by_name("email2").send_keys(company_details.email_2)
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(email3)
+        wd.find_element_by_name("email3").send_keys(company_details.email_3)
 
-    def add_phone_number(self, mobile_num="5476981465", work_num="3333", fax_num="4521754"):
+    def add_phone_number(self, company_details):
         wd = self.wd
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobile_num)
+        wd.find_element_by_name("mobile").send_keys(company_details.mobile_num)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(work_num)
+        wd.find_element_by_name("work").send_keys(company_details.work_num)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(fax_num)
+        wd.find_element_by_name("fax").send_keys(company_details.fax_num)
 
     def add_company(self, company_details):
         wd = self.wd
@@ -140,24 +139,24 @@ class Application:
         wd.find_element_by_name("home").clear()
         wd.find_element_by_name("home").send_keys(company_details.comHome)
 
-    def add_photo(self):
+    def add_photo(self, company_details):
         wd = self.wd
-        wd.find_element_by_xpath("/html/body/div/div[4]/form/input[7]").send_keys(image_path)
+        wd.find_element_by_xpath("/html/body/div/div[4]/form/input[7]").send_keys(company_details.image_path)
 
-    def add_full_name(self, firstname="ivan", middlename="ivanovich", lastname="raduk", nickname="irad"):
+    def add_full_name(self, company_details):
         wd = self.wd
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(company_details.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(company_details.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(company_details.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(company_details.nickname)
 
     def open_the_contact_creation_page(self):
         wd = self.wd
@@ -169,5 +168,3 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
-
-image_path = os.path.abspath(r'C:\Users\admin\Pictures\Screenshots\Снимок экрана (2).png')
--- test/test_add_contacts.py
+++ test/test_add_contacts.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from fixture.application import Application
 import pytest
-from model.company_details import CompanyDetails
+from model.company_details import *
 
 @pytest.fixture
 def app(request):
@@ -12,31 +12,30 @@ def app(request):
 def test_add_contact(app):
     app.login(username='admin', password='secret')
     app.open_the_contact_creation_page()
-    app.add_full_name(firstname='Nikita', middlename='Olegovich', lastname='Pavlov', nickname='NIKPAVLOV')
-    app.add_photo()
+    app.add_full_name(FullName(firstname='Nikita', middlename='Olegovich', lastname='Pavlov', nickname='NIKPAVLOV'))
+    app.add_photo(Image(image_path=r'C:\Users\admin\Pictures\Screenshots\Снимок экрана (2).png'))
     app.add_company(CompanyDetails(header='TEST', comName='New_comany', comAddress='yl.pysh', comHome='www.company.com'))
-    app.add_phone_number(mobile_num='786512', work_num='222', fax_num='125423')
-    app.add_email(email_one='easdq@mail.ru', email2='testtest@yandex.ru', email3='asdasdqwe@google.com')
-    app.add_homepage(homepage='vk.com')
-    app.add_date_birth(day='12', month='July', year='1990')
-    app.add_date_anniversary(a_day='23', a_month='April', a_year='2004')
+    app.add_phone_number(PhoneNomber(mobile_num='786512', work_num='222', fax_num='125423'))
+    app.add_email(Email(email_1='easdq@mail.ru', email_2='testtest@yandex.ru', email_3='asdasdqwe@google.com'))
+    app.add_homepage(Homepage(homepage='vk.com'))
+    app.add_date_birth(BirthDate(day='12', month='July', year='1990'))
+    app.add_date_anniversary(AnniversaryDate(a_day='23', a_month='April', a_year='2004'))
     app.select_a_group()
-    app.adding_secondary_data(address='Asdwqead', phone='342324', notes='fghrturturtuy ruub')
+    app.adding_secondary_data(SecondaryData(address='Asdwqead', phone='342324', notes='fghrturturtuy ruub'))
     app.logout()
 
 def test_add_contact2(app):
-    app.open_home_page()
     app.login(username='admin', password='secret')
     app.open_the_contact_creation_page()
-    app.add_full_name(firstname='Edward', middlename='Edward', lastname='Pavlov', nickname='Edward')
-    app.add_photo()
-    app.add_company(CompanyDetails(header='TEST', comName='New_comany', comAddress='yl.pysh', comHome='www.company.com'))
-    app.add_phone_number(mobile_num='32131', work_num='111', fax_num='4232521')
-    app.add_email(email_one='easdq@mail.ru', email2='testtest@yandex.ru', email3='asdasdqwe@google.com')
-    app.add_homepage(homepage='vk.com')
-    app.add_date_birth(day='1', month='July', year='1991')
-    app.add_date_anniversary(a_day='14', a_month='April', a_year='2001')
+    app.add_full_name(FullName(firstname='Ivan', middlename='Nikitovich', lastname='Melnikov', nickname='Yragan'))
+    app.add_photo(Image(image_path=r'C:\Users\admin\Pictures\Screenshots\Снимок экрана (2).png'))
+    app.add_company(CompanyDetails(header='TEST1', comName='New_comany1', comAddress='yl.pysh2', comHome='www.compan12312y.com'))
+    app.add_phone_number(PhoneNomber(mobile_num='7812312', work_num='22112', fax_num='1254124423'))
+    app.add_email(Email(email_1='e234234asdq@mail.ru', email_2='test123123test@yandex.ru', email_3='asda34234sdqwe@google.com'))
+    app.add_homepage(Homepage(homepage='v11312k.com'))
+    app.add_date_birth(BirthDate(day='13', month='July', year='1994'))
+    app.add_date_anniversary(AnniversaryDate(a_day='25', a_month='April', a_year='2005'))
     app.select_a_group()
-    app.adding_secondary_data(address='Asdwqead', phone='342324', notes='fghrturturtuy ruub')
+    app.adding_secondary_data(SecondaryData(address='Asdwqead', phone='342324', notes='fghrturturtuy ruub'))
     app.logout()
 
