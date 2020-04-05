c51efbe4ebfb06574fdaaa955a20bd4835aac9eb
admin <sirhjkru@gmail.com>
2020-04-05 20:50:04 +0300

    Все данные контакта перенес в отдин отлеьный класс

--- model/company_details.py
+++ model/company_details.py
@@ -1,58 +1,81 @@
 class CompanyDetails:
-    def __init__(self, header, comName, comAddress, comHome):
+    def __init__(self, header=None, comName=None, comAddress=None, comHome=None, address=None, phone=None, notes=None, a_day=None, a_month=None, a_year=None, day=None, month=None, year=None,
+                 homepage=None, email_1=None, email_2=None, email_3=None, mobile_num=None, work_num=None, fax_num=None, image_path=None, firstname=None, middlename=None, lastname=None, nickname=None):
         self.header = header
         self.comName = comName
         self.comAddress = comAddress
         self.comHome = comHome
-
-
-class SecondaryData:
-    def __init__(self, address, phone, notes):
         self.address = address
         self.phone = phone
         self.notes = notes
-
-
-class AnniversaryDate:
-    def __init__(self, a_day, a_month, a_year):
         self.a_day = a_day
         self.a_month = a_month
         self.a_year = a_year
-
-
-class BirthDate:
-    def __init__(self, day, month, year):
         self.day = day
         self.month = month
         self.year = year
-
-
-class Homepage:
-    def __init__(self, homepage):
         self.homepage = homepage
-
-
-class Email:
-    def __init__(self, email_1, email_2, email_3):
         self.email_1 = email_1
         self.email_2 = email_2
         self.email_3 = email_3
-
-class PhoneNomber:
-    def __init__(self, mobile_num, work_num, fax_num):
         self.mobile_num = mobile_num
         self.work_num = work_num
         self.fax_num = fax_num
-
-
-class Image:
-    def __init__(self, image_path):
         self.image_path = image_path
-
-
-class FullName:
-    def __init__(self, firstname, middlename, lastname, nickname):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
         self.nickname = nickname
+
+
+
+# class SecondaryData:
+#     def __init__(self, address, phone, notes):
+#         self.address = address
+#         self.phone = phone
+#         self.notes = notes
+#
+#
+# class AnniversaryDate:
+#     def __init__(self, a_day, a_month, a_year):
+#         self.a_day = a_day
+#         self.a_month = a_month
+#         self.a_year = a_year
+#
+#
+# class BirthDate:
+#     def __init__(self, day, month, year):
+#         self.day = day
+#         self.month = month
+#         self.year = year
+#
+#
+# class Homepage:
+#     def __init__(self, homepage):
+#         self.homepage = homepage
+#
+#
+# class Email:
+#     def __init__(self, email_1, email_2, email_3):
+#         self.email_1 = email_1
+#         self.email_2 = email_2
+#         self.email_3 = email_3
+#
+# class PhoneNomber:
+#     def __init__(self, mobile_num, work_num, fax_num):
+#         self.mobile_num = mobile_num
+#         self.work_num = work_num
+#         self.fax_num = fax_num
+#
+#
+# class Image:
+#     def __init__(self, image_path):
+#         self.image_path = image_path
+#
+#
+# class FullName:
+#     def __init__(self, firstname, middlename, lastname, nickname):
+#         self.firstname = firstname
+#         self.middlename = middlename
+#         self.lastname = lastname
+#         self.nickname = nickname
